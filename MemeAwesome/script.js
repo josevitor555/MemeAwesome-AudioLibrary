8a6e431abb033939b77ec9fef093edaa854597a4
José Vitor <127617992+josevitor555@users.noreply.github.com>
2023-04-06 13:16:19 -0300

    Add files via upload

---
+++ MemeAwesome/script.js
@@ -0,0 +1,85 @@
+let play_meme_1 = document.getElementById("meme-1");
+let play_meme_2 = document.getElementById("meme-2");
+let play_meme_3 = document.getElementById("meme-3");
+let play_meme_4 = document.getElementById("meme-4");
+let play_meme_5 = document.getElementById("meme-5");
+let play_meme_6 = document.getElementById("meme-6");
+let play_meme_7 = document.getElementById("meme-7");
+let play_meme_8 = document.getElementById("meme-8");
+let play_meme_9 = document.getElementById("meme-9");
+let play_meme_10 = document.getElementById("meme-10");
+let play_meme_11 = document.getElementById("meme-11");
+let play_meme_12 = document.getElementById("meme-12");
+
+
+function playAudio_meme_1() {
+    let audio = new Audio("audios/Bass Drum Impact Sound Effect.mp3");
+    audio.play();
+}
+play_meme_1.addEventListener('click', playAudio_meme_1);
+
+function playAudio_meme_2() {
+    let audio = new Audio("audios/aí aí zé da manga.mp3");
+    audio.play();
+}
+play_meme_2.addEventListener('click', playAudio_meme_2);
+
+function playAudio_meme_3() {
+    let audio = new Audio("audios/Musica da universal esttourada.mp3");
+    audio.play();
+}
+play_meme_3.addEventListener('click', playAudio_meme_3);
+
+function playAudio_meme_4() {
+    let audio = new Audio("audios/porn hub meme.mp3");
+    audio.play();
+}
+play_meme_4.addEventListener('click', playAudio_meme_4);
+
+function playAudio_meme_5() {
+    let audio = new Audio("audios/O Novo Gemidão - Áudio para edição YouTube e whatsapp.mp3");
+    audio.play();
+}
+play_meme_5.addEventListener('click', playAudio_meme_5);
+
+function playAudio_meme_6() {
+    let audio = new Audio("audios/Ain Nobruzera apelão(ESTOURADO).mp3");
+    audio.play();
+}
+play_meme_6.addEventListener('click', playAudio_meme_6);
+
+function playAudio_meme_7() {
+    let audio = new Audio("audios/PEGUE NA MINHA POMBAAAAAA - áudio meme.mp3");
+    audio.play();
+}
+play_meme_7.addEventListener('click', playAudio_meme_7);
+
+function playAudio_meme_8() {
+    let audio = new Audio("audios/musica do pou estourado.mp3");
+    audio.play();
+}
+play_meme_8.addEventListener('click', playAudio_meme_8);
+
+function playAudio_meme_9() {
+    let audio = new Audio("audios/Teto - Dia Azul (Ft. Pou).mp3");
+    audio.play();
+}
+play_meme_9.addEventListener('click', playAudio_meme_9);
+
+function playAudio_meme_10() {
+    let audio = new Audio("audios/Meme do cebolinha xingando.mp3");
+    audio.play();
+}
+play_meme_10.addEventListener('click', playAudio_meme_10);
+
+function playAudio_meme_11() {
+    let audio = new Audio("audios/Abre essa porta carai que eu te mato diabo.mp3");
+    audio.play();
+}
+play_meme_11.addEventListener('click', playAudio_meme_11);
+
+function playAudio_meme_12() {
+    let audio = new Audio("audios/Me dá minha sacola fí duma égua.mp3");
+    audio.play();
+}
+play_meme_12.addEventListener('click', playAudio_meme_12);
